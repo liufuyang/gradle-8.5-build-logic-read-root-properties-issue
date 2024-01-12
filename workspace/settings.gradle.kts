88e0d46400d7e11ff3a25473cf11b3744c79bbce
Fuyang Liu <liufuyang@users.noreply.github.com>
2024-01-12 16:20:22 +0100

    Add files via upload

---
+++ workspace/settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "g1"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+// For sub projects, and we have the power  of Kotlin!
+// include all projects from ../java folder
+val ignoredSubFolders = listOf("gradle", "build")
+file("../java").listFiles()
+        .filter{ it.isDirectory && !it.isHidden && !it.name.startsWith(".") && !ignoredSubFolders.contains(it.name) }
+        .forEach {
+            include(":${it.name}")
+            project(":${it.name}").projectDir = file("../java/${it.name}")
+        }
